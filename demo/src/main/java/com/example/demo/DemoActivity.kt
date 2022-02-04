9c85cfe58298c85fbfac8f05300fbdb261839ca8
Chinmay <chinmaydash11@gmail.com>
2022-02-04 14:26:20 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/DemoActivity.kt
@@ -0,0 +1,93 @@
+package com.example.demo
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.getvisitapp.google_fit.IntiateSdk
+import com.getvisitapp.google_fit.event.MessageEvent
+import com.getvisitapp.google_fit.event.VisitEventType
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+
+class DemoActivity : AppCompatActivity() {
+    var TAG = this.javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            //        val prodMagicLink = "https://vsyt.me/m/1XFjA45h"
+            val magicLink = "https://tata-aig.getvisitapp.xyz"
+            val baseUrlOfMagicLink = "https://tata-aig.getvisitapp.xyz/"
+//
+            val default_client_id =
+                "1050989325945-11eh51j1d0ocl945gmk583ai35ospog3.apps.googleusercontent.com"
+
+
+
+
+            IntiateSdk.s(this, false, magicLink, baseUrlOfMagicLink, default_client_id)
+
+        }
+
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: MessageEvent?) {
+        event?.let { eventType ->
+            Log.d(TAG, "event:${event.eventType}")
+
+
+            when (eventType.eventType) {
+                VisitEventType.AskForFitnessPermission -> {
+
+                }
+                VisitEventType.AskForLocationPermission -> {
+
+
+                }
+                VisitEventType.FitnessPermissionGranted -> {
+
+
+                }
+                is VisitEventType.RequestHealthDataForDetailedGraph -> {
+
+                    val graphEvent =
+                        event.eventType as VisitEventType.RequestHealthDataForDetailedGraph
+
+                }
+                is VisitEventType.StartVideoCall -> {
+                    val callEvent =
+                        event.eventType as VisitEventType.StartVideoCall
+
+
+                }
+            }
+
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        //unregister first before registering again.
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this)
+        }
+        EventBus.getDefault().register(this)
+    }
+
+
+    override fun onDestroy() {
+        EventBus.getDefault().unregister(this)
+        super.onDestroy()
+    }
+
+
+}
