4a4b4eb9d1d860a450836025795e995a45a025bc
Chinmay <chinmaydash11@gmail.com>
2022-02-18 19:51:44 +0530

    updated library dependency

--- demo/src/main/java/com/example/demo/DemoActivity.kt
+++ demo/src/main/java/com/example/demo/DemoActivity.kt
@@ -5,18 +5,33 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import com.getvisitapp.google_fit.IntiateSdk
+import com.getvisitapp.google_fit.data.VisitStepSyncHelper
 import com.getvisitapp.google_fit.event.ClosePWAEvent
 import com.getvisitapp.google_fit.event.MessageEvent
 import com.getvisitapp.google_fit.event.VisitEventType
+import com.getvisitapp.google_fit.util.GoogleFitAccessChecker
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
 //9978900304
-class DemoActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
     var TAG = this.javaClass.simpleName
+
+
+    lateinit var checker: GoogleFitAccessChecker
+    lateinit var switch: Switch
+
+    val tataAIG_base_url = "https://uathealthvas.tataaig.com"
+    val tataAIG_auth_token = "Basic Z2V0X3Zpc2l0OkZoNjh2JHdqaHU4WWd3NiQ="
+
+
+    val default_client_id =
+        "1050989325945-11eh51j1d0ocl945gmk583ai35ospog3.apps.googleusercontent.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +39,16 @@ class DemoActivity : AppCompatActivity() {
 
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            val magicLink = "https://tata-aig.getvisitapp.xyz"
-            val baseUrlOfMagicLink = "https://tata-aig.getvisitapp.xyz/"
-//
-            val default_client_id =
-                "1050989325945-11eh51j1d0ocl945gmk583ai35ospog3.apps.googleusercontent.com"
-
-
-
-
-            IntiateSdk.s(this, false, magicLink, baseUrlOfMagicLink, default_client_id)
+            init()
 
         }
+
+        switch = findViewById<Switch>(R.id.switch1)
+
+        checker = GoogleFitAccessChecker(this)
+
+        val syncStepHelper = VisitStepSyncHelper(context = this, default_client_id)
+        syncStepHelper.syncSteps()
 
     }
 
@@ -47,30 +60,67 @@ class DemoActivity : AppCompatActivity() {
 
 
             when (eventType.eventType) {
-                VisitEventType.AskForFitnessPermission -> {}
-                VisitEventType.AskForLocationPermission -> {}
-                VisitEventType.FitnessPermissionGranted -> {}
+                VisitEventType.AskForFitnessPermission -> {
+
+                }
+                VisitEventType.AskForLocationPermission -> {
+
+
+                }
+                VisitEventType.FitnessPermissionGranted -> {
+
+
+                }
                 is VisitEventType.RequestHealthDataForDetailedGraph -> {
+
                     val graphEvent =
                         event.eventType as VisitEventType.RequestHealthDataForDetailedGraph
+
                 }
                 is VisitEventType.StartVideoCall -> {
                     val callEvent =
                         event.eventType as VisitEventType.StartVideoCall
+
+
                 }
                 is VisitEventType.HRA_Completed -> {
-                    Handler(Looper.getMainLooper()).postDelayed({
 
+
+                }
+                is VisitEventType.GoogleFitConnectedAndSavedInPWA -> {
+                    Handler(Looper.getMainLooper()).postDelayed({
                         //passing event to Visit PWA to close itself
                         EventBus.getDefault().post(ClosePWAEvent())
 
 
                     }, 200)
                 }
+                is VisitEventType.HRAQuestionAnswered -> {
+                    // can be used for analytics events
+                    val hraQuestionEvent = event.eventType as VisitEventType.HRAQuestionAnswered
+                    Log.d(
+                        "mytag",
+                        "current:${hraQuestionEvent.current} total:${hraQuestionEvent.total}"
+                    )
+                }
             }
 
         }
 
+    }
+
+    fun init() {
+//        val magicLink = "https://tata-aig.getvisitapp.xyz"
+        val magicLink =
+            "https://tata-aig.getvisitapp.xyz/sso?userParams=0UQpL6UCwpWLwQjwMZGOEz1Qt3lhN3BbQXuvTBhsRo4gTy6kz7JftAFCVY14rGwIaGAOu8jRbe8cOgjKxIR1cjYI0MlwkOLcDDxLfYZzMiM9Tkks2JwwC4k12kQsO5O6J0VPKTMhSfz6qzngMQmLx8d2tkocMELoNF8_ABWuAxwc4SB0r4v8wAVGsfAjEsktS8R09ZbVfVrXdCnV1qPmFw&clientId=tata-aig-a8b455"
+        IntiateSdk.s(
+            this,
+            false,
+            magicLink,
+            tataAIG_base_url,
+            tataAIG_auth_token,
+            default_client_id
+        )
     }
 
     override fun onStart() {
@@ -81,6 +131,23 @@ class DemoActivity : AppCompatActivity() {
             EventBus.getDefault().unregister(this)
         }
         EventBus.getDefault().register(this)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        switch.setOnCheckedChangeListener(null)
+        switch.isChecked = checker.checkGoogleFitAccess()
+        switch.setOnCheckedChangeListener(this)
+    }
+
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        if (isChecked) {
+            init()
+        } else {
+            checker.revokeGoogleFitPermission(default_client_id)
+        }
     }
 
 
